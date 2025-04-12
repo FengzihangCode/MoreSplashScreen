87c8b9d8a827af8ff83a7514625b54d312e156f4
DannyFeng <FengzihangDanny@iCloud.com>
2025-04-12 18:43:24 +0800

    增加 Adobe Photoshop 外观的启动画面

---
+++ MoreSplashScreen/Views/SplashScreens/AdobePhotoshopSplashScreen.xaml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Windows;
+
+namespace MoreSplashScreen
+{
+    public partial class AdobePhotoshopSplashScreen : Window
+    {
+        public ISplashService SplashService { get; }
+        private bool _closed;
+
+        public AdobePhotoshopSplashScreen(ISplashService splashService)
+        {
+            SplashService = splashService;
+            SplashService.SplashEnded += OnSplashEnded;
+
+            InitializeComponent();
+        }
+
+        private void OnSplashEnded(object? sender, EventArgs e)
+        {
+            SplashService.SplashEnded -= OnSplashEnded;
+            if (!_closed)
+            {
+                Dispatcher.InvokeAsync(Close);
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _closed = true;
+            base.OnClosed(e);
+        }
+    }
+}
